2a1ca58ca9602f39e2e2e9a6b5ea7b7d846fdbc2
Babak.Fakhary@gmail.com <Babak.Fakhary@gmail.com>
2025-10-05 17:54:16 +0330

    اصلاح ساختار پوشه

--- Docker/Session1/app.py
+++ Docker/Session1/app.py
@@ -1,30 +1,17 @@
-#                                                                                  به نام خدا
-
-# ===========================================================================
-#                            اولین جلسه  داکر
-# ===========================================================================
-
+#                                                                           به نام خدا
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f'سلام! من در Docker اجرا می‌شم! 🐳'
-
-@app.route('/info')
-def info():
-    return {
-        'python_version': os.environ.get('PYTHON_VERSION', '3.12.6'),
-        'hostname': os.environ.get('HOSTNAME', 'unknown')
-    }
+    return 'Hello from Docker!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
-
-# ------------------------------------------------------------
-#  برای اجرای کد دستور زیر را اجرا می کنیم
-#  docker run -it -p 5000:5000 -v "/D:/Projects/Visual Code/Python/DeepSeekSamples/AI-Journey-with-DeepSeek/Docker/Session1":/app python:3.12.6-slim sh
-# ------------------------------------------------------------    
+# ===================================================
+# For Build Image 
+# docker build -t my-python-app .
+# نقطه در پایان دستور جز دستور می باشد
+# ===================================================
